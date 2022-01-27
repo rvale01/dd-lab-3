18891c9f1e601e2e66741534239ddeedbdee0aa4
v2-ronchi <valentina2.ronchi@live.uwe.ac.uk>
2022-01-27 01:24:17 +0000

    Fixed the output of the LEDs

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ if __name__ == '__main__':
     while True:
         if(int(gpio.read(GPIO_INPUT.ADC7)) == 1):
             time.sleep(2) #green and orange
-            gpio.write(GPIO_OUTPUT.ADC0, GPIO_OUTPUT.LOW)
-            gpio.write(GPIO_OUTPUT.ADC1, GPIO_OUTPUT.HIGH)
-            time.sleep(3) #red
             gpio.write(GPIO_OUTPUT.ADC0, GPIO_OUTPUT.HIGH)
             gpio.write(GPIO_OUTPUT.ADC1, GPIO_OUTPUT.LOW)
+            time.sleep(3) #red
+            gpio.write(GPIO_OUTPUT.ADC0, GPIO_OUTPUT.LOW)
+            gpio.write(GPIO_OUTPUT.ADC1, GPIO_OUTPUT.HIGH)
             time.sleep(3) #red and orange      
             gpio.write(GPIO_OUTPUT.ADC1, GPIO_OUTPUT.LOW)
-            gpio.write(GPIO_OUTPUT.ADC0, GPIO_OUTPUT.LOW)
+            gpio.write(GPIO_OUTPUT.ADC0, GPIO_OUTPUT.LOW) 
             time.sleep(3)
         else:
-            gpio.write(GPIO_OUTPUT.ADC0, GPIO_OUTPUT.LOW)
+            gpio.write(GPIO_OUTPUT.ADC0, GPIO_OUTPUT.HIGH) 
             gpio.write(GPIO_OUTPUT.ADC1, GPIO_OUTPUT.HIGH)
 
 
